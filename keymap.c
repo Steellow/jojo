0c2777bde572b2485657eecb30d2d6611ee00be1
Hannes Kinnunen <hankilapio@gmail.com>
2023-07-26 15:55:02 +0300

    Change oled to rerorero

--- keymap.c
+++ keymap.c
@@ -159,17 +159,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-static void render_logo(void) {
-	
-    static const char PROGMEM logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(logo, false);
-}
-
 char keylog_str[24] = {};
 char keylogs_str[21] = {};
 int keylogs_str_idx = 0;
@@ -216,41 +205,11 @@ const char *read_keylogs(void) {
   return keylogs_str;
 }
 
-static void render_status(void) {
-    oled_write_P(PSTR("layer "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("QWERTY"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower "), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("Raise "), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("Adjust"), false);
-            break;
-        default:
-            sprintf(layer_misc, "MO(%01d) ", get_highest_layer(layer_state));
-	    oled_write(layer_misc, false);
-            break;
-    }
-
-    oled_write_P(PSTR("  "), false);
-    sprintf(wpm, "%03d", get_current_wpm());
-    oled_write(wpm, false);
-    oled_write_P(PSTR(" wpm\n"), false);
-}
-
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    render_status();
-  } else {
-    render_logo();
-  }
-    return false;
+
+    oled_write("rerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorerorero", false);
+  
+  return false;
 }
 
 #endif // OLED_ENABLE
